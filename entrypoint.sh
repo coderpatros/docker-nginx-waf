6fb7a78702bfb96312d6c127ba676f5e809cdb75
Patrick Dwyer <pat@patdwyer.com.au>
2019-08-27 08:21:59 +1000

    Make nginx log directory if it doesn't exist on startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,7 @@
 sed -i "s/SecAuditEngine \S*/SecAuditEngine $SEC_AUDIT_ENGINE/" /etc/nginx/modsec/modsecurity.conf
 sed -i "s/SecRuleEngine \S*/SecRuleEngine $SEC_RULE_ENGINE/" /etc/nginx/modsec/modsecurity.conf
 
+# make directory /var/log/nginx if needed
+mkdir --parents /var/log/nginx
+
 nginx -g "daemon off;"
